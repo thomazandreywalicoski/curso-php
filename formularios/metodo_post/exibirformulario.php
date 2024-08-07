a0e73ee4bb0d0df68e4d1aa20dee9e3f879f1a78
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2024-08-07 11:23:00 -0300

    Método POST

---
+++ formularios/metodo_post/exibirformulario.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "O nome é: " . $_POST['nome'] . "<br>";
+echo "A idade é: " . $_POST['idade'] . "<br>";
+echo "O e-mail é: " . $_POST['email'] . "<br>";
+
+?>
---
+++ formularios/metodo_post/formulario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <form action="exibirformulario.php" method="post">
+        Nome: <input type="text" name="nome"><br><br>
+        Idade: <input type="number" name="idade"><br><br>
+        E-mail: <input type="text" name="email"><br><br>
+        <button type="submit">Enviar</button>
+    </form>
+
+</body>
+</html>
