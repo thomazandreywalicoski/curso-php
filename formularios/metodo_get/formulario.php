f43484dcb396051d7008e2012dfc7a162f306a1d
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2024-08-07 10:51:47 -0300

    Método GET

---
+++ formularios/metodo_get/formulario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <form action="exibirformulario.php" method="get">
+        Nome: <input type="text" name="nome"><br><br>
+        Idade: <input type="number" name="idade"><br><br>
+        E-mail: <input type="text" name="email"><br><br>
+        <button type="submit">Enviar</button>
+    </form>
+
+<!-- O método GET envia os dados pela URL -->
+
+</body>
+</html>
+
