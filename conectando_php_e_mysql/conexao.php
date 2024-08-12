c55a74a5c1ffe5be71fc356d6fd2d751b8297559
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2024-08-12 11:03:07 -0300

    Conectando PHP e MySQL

---
+++ conectando_php_e_mysql/conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = "localhost";
+$bancodedados = "livraria";
+$usuario = "root";
+$senha = "";
+
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+
+if($mysqli->connect_errno) {
+    echo "Falha ao conectar: (" . $mysqli->connect_errno . ")" . $mysqli->connect_error;
+}
+
+?>
