a1bac6d34b58b079d54f2130ccd5571ae47784ba
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2024-08-17 17:45:11 -0300

    Adicionado JS e formulario de edição em contrução

--- crud_produtos/conexao.php
+++ crud_produtos/conexao.php
@@ -10,3 +10,7 @@ $mysqli = new mysqli($host, $usuario, $senha, $db);
 if($mysqli->connect_errno) {
     die("Falha na conexao com o banco de dados"); 
 }
+
+function formatar_data($data) {
+    return implode('/', array_reverse(explode('-', $data)));
+}
--- crud_produtos/script/abrircadastrarprodutos.js
+++ crud_produtos/script/abrircadastrarprodutos.js
@@ -10,11 +10,12 @@ function fecharCadastrarProdutos() {
     let abrirModalCadastrarProduto = document.querySelector('.fundo-modal')
 
     abrirModalCadastrarProduto.style.display = 'none';
+    window.location.reload();
 }
 
 
 
-
+/* 
 function abrirEditarProdutos() {
     
     let abrirModalEditarProduto = document.querySelector('.fundo-modal-editar')
@@ -28,12 +29,43 @@ function fecharEditarProdutos() {
 
     abrirModalEditarProduto.style.display = 'none';
 }
+*/
 
-document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
-    if (id) {
-        // Código para abrir o modal e carregar dados do produto
-        openModalWithProduct(id);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const abrirModalEditarProduto = document.getElementById('modal');
+
+    // Verificar o estado do modal no localStorage
+    if (localStorage.getItem('modalOpen') === 'true') {
+        abrirModalEditarProduto.style.display = 'flex';
     }
+
+    // Abrir o modal
+    abrirEditarProdutos = function() {
+        abrirModalEditarProduto.style.display = 'flex';
+        localStorage.setItem('modalOpen', 'true');
+    }
+
+    // Fechar o modal
+    fecharEditarProdutos = function() {
+        abrirModalEditarProduto.style.display = 'none';
+        localStorage.setItem('modalOpen', 'false');
+
+        const url = new URL(window.location);
+        url.search = ''; // Remove parâmetros de consulta
+        url.hash = '';   // Remove o fragmento da URL (hash)
+        window.history.replaceState({}, document.title, url.toString());
+        window.location.reload();
+    }
+
+    document.getElementById('salvar-edicao').addEventListener('click', function() {
+        localStorage.removeItem('modalOpen'); // Remove o item do localStorage
+        window.location.reload();
+        
+    });
+
+
 });
+
+
