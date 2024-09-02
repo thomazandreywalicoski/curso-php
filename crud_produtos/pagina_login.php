3afba575f0d06e1229a2eaffad42855cf78b2a7d
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2024-09-02 14:27:46 -0300

    Adicionado uma tela de login para acessar o sistema

---
+++ crud_produtos/pagina_login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilo/pagina_login/login.css">
+    <title>Entrar</title>
+</head>
+<body>
+    
+    <div class="login-c">
+    
+        <div class="login">
+            <form action="" method="post" class="formulario-login">
+                <div class="formulario-login-img">
+                    <img src="logo/logo-2.png" alt="">
+                </div>
+                <div class="formulario-login-titulo">
+                    <h1>Painel de Administração</h1>
+                </div>
+                <div class="formulario-login-descricao">
+                    <p>Preencha seus dados para entrar</p>
+                </div>
+                <div class="formulario-login-input">
+                    <input type="text" placeholder="E-mail">
+                    <input type="password" placeholder="Senha">
+                </div>
+                <div class="formulario-login-btn">
+                    <button type="submit">Entrar</button>
+                </div>
+            </form>
+            <div class="login-imagem-c">
+                <div class="login-imagem">
+                    <img src="logo/mao.png" alt="">
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
