355e9f05ac16dd1c0b019fc258a5b448c40d20cc
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2024-09-02 15:23:57 -0300

    Adicionado menu de opções e perfil do usuário

--- crud_produtos/produtos.php
+++ crud_produtos/produtos.php
@@ -10,6 +10,54 @@
 <body>
 
 
+
+    <div class="menu-administrador-c">
+        <div class="menu-administrador">
+
+            <div class="menu-opcoes-c">
+                <div class="menu-opcoes">
+
+                    <div class="menu-opcoes-opcao-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff">
+                            <path d="M200-80q-33 0-56.5-23.5T120-160v-480q0-33 23.5-56.5T200-720h80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720h80q33 0 56.5 23.5T840-640v480q0 33-23.5 56.5T760-80H200Zm0-80h560v-480H200v480Zm280-240q83 0 141.5-58.5T680-600h-80q0 50-35 85t-85 35q-50 0-85-35t-35-85h-80q0 83 58.5 141.5T480-400ZM360-720h240q0-50-35-85t-85-35q-50 0-85 35t-35 85ZM200-160v-480 480Z"/>
+                        </svg>
+                        <p>Produtos</p>
+                    </div>
+                    
+                    <div class="menu-opcoes-opcao">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff">
+                            <path d="M160-80v-440H80v-240h208q-5-9-6.5-19t-1.5-21q0-50 35-85t85-35q23 0 43 8.5t37 23.5q17-16 37-24t43-8q50 0 85 35t35 85q0 11-2 20.5t-6 19.5h208v240h-80v440H160Zm400-760q-17 0-28.5 11.5T520-800q0 17 11.5 28.5T560-760q17 0 28.5-11.5T600-800q0-17-11.5-28.5T560-840Zm-200 40q0 17 11.5 28.5T400-760q17 0 28.5-11.5T440-800q0-17-11.5-28.5T400-840q-17 0-28.5 11.5T360-800ZM160-680v80h280v-80H160Zm280 520v-360H240v360h200Zm80 0h200v-360H520v360Zm280-440v-80H520v80h280Z"/>
+                        </svg>
+                        <p>Promoções</p>
+                    </div>
+
+                    <div class="menu-opcoes-opcao">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff">
+                            <path d="M160-240v-320 320Zm0 80q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800l80 160h120l-80-160h80l80 160h120l-80-160h80l80 160h120l-80-160h120q33 0 56.5 23.5T880-720v160H160v320h320v80H160Zm400 40v-123l221-220q9-9 20-13t22-4q12 0 23 4.5t20 13.5l37 37q8 9 12.5 20t4.5 22q0 11-4 22.5T903-340L683-120H560Zm300-263-37-37 37 37ZM620-180h38l121-122-18-19-19-18-122 121v38Zm141-141-19-18 37 37-18-19Z"/>
+                        </svg>
+                        <p>Banners</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="perfil-usuario">
+                <div class="perfil-usuario-nome">
+                    <p>Bem Estar</p>
+                </div>
+                <div class="perfil-usuario-img">
+                    <img src="logo/logo-2.png" alt="">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
     <!-------------------------------------------------------------------------------------------------->
     <!--                                         DASHBOARD                                            -->
     <!-------------------------------------------------------------------------------------------------->
