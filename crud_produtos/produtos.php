d1bde3ce4e0e55c387a5fa5c2e636cb7e80acb7f
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2024-09-04 11:08:50 -0300

    Adicionado responsividade a tabela de produtos cadastrados

--- crud_produtos/produtos.php
+++ crud_produtos/produtos.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="./estilo/modal_cadastrar_editat_deletar_produto/estilo_modal.css">
     <link rel="stylesheet" href="./responsividade/dashboard.css">
     <link rel="stylesheet" href="./responsividade/painel_de_controle.css">
+    <link rel="stylesheet" href="./responsividade/tabela_produtos.css">
+    <link rel="stylesheet" href="./responsividade/paginacao.css">
     <title>Painel de Administração</title>  
 </head>
 <body>
@@ -601,22 +603,21 @@
 
                         <th class="coluna-tabela">
                             <div class="informacao-tabela">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff">
-                                    <path d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"/>
-                                </svg>
-                                <p>Categoria</p>
-                            </div> <!-- .informacao-tabela -->
-                        </th> <!-- .coluna-tabela -->
-
-                        <th class="coluna-tabela">
-                            <div class="informacao-tabela">
                                 <svg class="icone-coluna" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff">
                                     <path d="M446-80q-15 0-30-6t-27-18L103-390q-12-12-17.5-26.5T80-446q0-15 5.5-30t17.5-27l352-353q11-11 26-17.5t31-6.5h287q33 0 56.5 23.5T879-800v287q0 16-6 30.5T856-457L503-104q-12 12-27 18t-30 6Zm0-80 353-354v-286H513L160-446l286 286Zm253-480q25 0 42.5-17.5T759-700q0-25-17.5-42.5T699-760q-25 0-42.5 17.5T639-700q0 25 17.5 42.5T699-640ZM480-480Z"/>
                                 </svg>
                                 <p>Marca</p>
                             </div> <!-- .informacao-tabela -->
                         </th> <!-- .coluna-tabela -->
-                    
+
+                        <th class="coluna-tabela">
+                            <div class="informacao-tabela">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff">
+                                    <path d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"/>
+                                </svg>
+                                <p>Categoria</p>
+                            </div> <!-- .informacao-tabela -->
+                        </th> <!-- .coluna-tabela -->
 
                         <th class="coluna-tabela">
                             <div class="informacao-tabela">
@@ -709,11 +710,11 @@
                                             </td>
 
                                             <td class="dado-produto">
-                                                <?php echo $produto['categoria']; ?>
+                                                <?php echo $produto['marca']; ?>
                                             </td>
 
                                             <td class="dado-produto">
-                                                <?php echo $produto['marca']; ?>
+                                                <?php echo $produto['categoria']; ?>
                                             </td>
 
                                             <td class="dado-produto">
